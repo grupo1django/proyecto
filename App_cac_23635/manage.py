6b461a2d40c9493e633edc63492adbdce4c52097
Victoria Avalos <inessavalos14@gmail.com>
2023-09-19 14:21:35 -0300

    "corrigiendo"

--- App_cac_23635/manage.py
+++ App_cac_23635/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'App_Saint.settings','App_cac_23635.urls')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'App_Saint.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
